49d91d0f125261b40772391970c9ef828c19f065
Nina Bobyleva <2847171@mail.ru>
2025-05-15 17:41:18 +0300

    Изменила стили в компонентах модального окна

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -2,6 +2,6 @@ import { Navigate, Outlet } from "react-router-dom";
 import { paths } from "../../paths";
 
 export function PrivateRoute() {
-  const user = false;
+  const user = true;
   return user ? <Outlet /> : <Navigate to={paths.SIGN_IN} />;
 }
